75bd8d8736086acfc33d69efcbe7ffbb84ad61e8
Oskar <ners.kontakt@wp.pl>
2024-09-11 11:02:46 +0200

    small change to update username also in user docs collection

--- src/app/services/update-profile.service.ts
+++ src/app/services/update-profile.service.ts
@@ -1,26 +1,36 @@
 import { Injectable, inject } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
+import { Firestore, doc } from '@angular/fire/firestore';
 import { updateProfile } from '@firebase/auth';
+import { updateDoc } from '@firebase/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UpdateProfileService {
   firebaseAuth = inject(Auth);
+  firestore = inject(Firestore);
 
   //   Zaaktualizowanie profilu nowym username
-  updateUserProfile(newUsername: string | null) {
+  async updateUserProfile(newUsername: string | null) {
     const currentUser = this.firebaseAuth.currentUser;
+
     if (currentUser) {
-      updateProfile(currentUser, {
-        displayName: newUsername,
-      })
-        .then(() => {
-          console.log('Profile updated! Your new username --> ' + newUsername);
-        })
-        .catch((err) => {
-          'Profile update failed ' + err.message;
+      const userDocRef = doc(this.firestore, 'users', currentUser.uid);
+
+      try {
+        await updateProfile(currentUser, {
+          displayName: newUsername,
         });
+
+        await updateDoc(userDocRef, {
+          username: newUsername,
+        });
+
+        console.log('Username updated successfully.');
+      } catch (err) {
+        console.error('Profile update failed: ' + err);
+      }
     }
   }
 }
